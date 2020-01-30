7a5c554987863b346ba7e15f21cdcf533d942952
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-30 17:07:33 +0100

    compaiono i numeri, i quadrati si colorano, ma manca qualcosa

--- js/script.js
+++ js/script.js
@@ -5,19 +5,26 @@
 // I quadrati fateli prima a mano e poi con javascript.
 
 $(document).ready(function() {
-  $('.square').click(
-    $.ajax(
-      {
-        url: 'https://flynn.boolean.careers/exercises/api/random/int',
-        method: "GET",
-        success: function(data, stato) {
-          console.log(data);
-          // $('#risultati').html(data);
-        },
-        error: function(richiesta, stato, errori) {
-          alert('E\' avvenuto un errore.' + errori);
+  $('.square').click(function() {
+      $.ajax(
+        {
+          url: 'https://flynn.boolean.careers/exercises/api/random/int',
+          method: "GET",
+          success: function(data, stato) {
+            if (data.response <= 5) {
+              $('.square').addClass('yellow');
+              $('.square').text(data.response);
+            } else {
+              $('.square').addClass('red');
+              $('.square').text(data.response);
+            }
+
+          },
+          error: function(richiesta, stato, errori) {
+            alert('E\' avvenuto un errore.' + errori);
+          }
         }
-      }
-    )
+      )
+    }
   );
 });
